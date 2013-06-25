70e672c2411dc3868282747d1081188d8311beda
Yacin Bahi <yacin@sensr.net>
2013-06-24 18:17:59 -0700

    fix refresh

--- lib/stripemetrics/client/refresh.rb
+++ lib/stripemetrics/client/refresh.rb
@@ -2,7 +2,7 @@ module Stripemetrics
   class Client
     module Refresh
       def refresh
-        response = post('/imports/refresh', :require_auth => true)
+        response = post('imports/refresh', :require_auth => true)
         raise TargetError if response.status == 404
         raise AuthError if response.status == 401
         (response.status == 200) ? 'success' : 'failure'
