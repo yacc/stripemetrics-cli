499832533645ce7a7eb0c6e2919985ffad4b16d9
Yacin Bahi <yacin@sensr.net>
2013-08-05 15:21:42 -0700

    Fix display of metrics

--- lib/stripemetrics/client/metric.rb
+++ lib/stripemetrics/client/metric.rb
@@ -12,18 +12,18 @@ module Stripemetrics
 
       def print(metrics, options={},args={})
         table(:border => true) do
-          header
+          header(metrics.first['this_month_ts'],metrics.first['last_month_ts'])
           metrics.each do |metric|
             display_row(metric) if (args.include?(metric['_type'])   || options[:all])
           end
         end
       end
 
-      def header
+      def header(month1,month2)
          row do
            column 'Metric', :width => 15,       :align => 'left'
-           column 'This month', :width => 11,   :align => 'right'
-           column 'Last month', :width => 11,   :align => 'right'
+           column "This month,#{month1}", :width => 11,   :align => 'right'
+           column "Last month,#{month2}", :width => 11,   :align => 'right'
            column '% change', :width => 11,     :align => 'right'
            column 'TSM Average', :width => 11,  :align => 'right'
            column 'Goal', :width => 11,         :align => 'right'
@@ -35,19 +35,18 @@ module Stripemetrics
            column metric['name']
            column with_unit(metric['_type'],metric['this_month'])
            column with_unit(metric['_type'],metric['last_month'])
-           column rounded_percent(metric['change'])
-           column rounded_percent(metric['tsm_avrg'])
+           column rounded_percent(metric['_type'],metric['change'])
+           column rounded_percent(metric['_type'],metric['tsm_avrg'])
            column metric['goal']
          end        
       end
 
-      def rounded_percent(val)
-        '%.2f%' % (val*100)
+      def rounded_percent(type,val)
+        '%+.2f%' % (val*100)
       end
 
       def with_unit(type,val)
-         unit = '$' if type.downcase =~ /revenue/
-         "#{unit}#{val}"     
+         (type.downcase =~ /revenue/) ? "$#{val}" : "#{val.to_i}"
       end
 
     end
